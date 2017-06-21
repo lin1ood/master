bb21326303b20feac40713e58056e8299a3f367b
Linwood Lassiter <linwoodlassiter@Linwoods-MBP.PK5001Z>
2017-06-21 10:33:58 -0400

    created game logic

--- fundamentals/memory_game/js/main.js
+++ fundamentals/memory_game/js/main.js
@@ -1,7 +1,21 @@
 console.log("Up and Running!!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+} 
+
+
